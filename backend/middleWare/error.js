9894555b1df3c1fd641974837b63d5e2d0c527d2
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-02-05 23:41:17 +0530

    error hanlder

--- backend/middleWare/error.js
+++ backend/middleWare/error.js
@@ -4,7 +4,7 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
-    console.log("hello");
+ 
      // Wrong Mongodb Id error for product 
     if (err.name === "CastError") {
         const message = `Resource not found. Invalid: ${err.path}`;
