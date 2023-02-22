e21855e34de5f1e06db61223deb7e14085b676a2
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-02-22 08:50:00 +0530

    login form added

--- frotend/src/component/User/LoginSignUp.jsx
+++ frotend/src/component/User/LoginSignUp.jsx
@@ -13,6 +13,9 @@ function LoginSignUp() {
     const [loginEmail , setLoginEmail] = useState("");
     const [loginPassword , setLoginPassWord] = useState("");
 
+    const loginTab  = useRef(null);
+    const registerTab  = useRef(null);
+    const switchTab = useRef(null);
 
   return (
     <>
@@ -23,8 +26,91 @@ function LoginSignUp() {
               <p>LOGIN</p>
               <p>REGISTER</p>
             </div>
-            <button ></button>
+            <button ref={switchTab}></button>
           </div>
+
+          <form
+            className="loginForm"
+            ref={loginTab}
+            onSubmit={handleLoginSubmit}
+          >
+            <div className="loginEmail">
+              <MailOutlineIcon />
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                value={loginEmail}
+                onChange={(e) => setLoginEmail(e.target.value)}
+              />
+            </div>
+            <div className="loginPassword">
+              <LockOpenIcon />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={loginPassword}
+                onChange={(e) => setLoginPassWord(e.target.value)}
+              />
+            </div>
+            <Link to="/password/forgot">Forget Password ?</Link>
+            <button className="loginBtn">Login</button>
+          </form>
+
+          {/* signUp box */}
+
+          <form
+            ref={registerTab}
+            onSubmit={handleSignUpSubmit}
+            className="signUpForm"
+            encType="multipart/form-data" // this is for file upLoad
+          >
+            <div className="signUpName">
+              <FaceIcon />
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={registerDataChange}
+              />
+            </div>
+            <div className="signUpEmail">
+              <MailOutlineIcon />
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                name="email"
+                value={email}
+                onChange={registerDataChange}
+              />
+            </div>
+
+            <div className="signUpPassword">
+              <LockOpenIcon />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                name="password"
+                onChange={registerDataChange}
+              />
+            </div>
+
+            <div id="registerImage">
+              <img src={avatarPreview} alt="Avatar Preview" />
+              <input
+                type="file"
+                name="avatar"
+                accept="image/*" // all type image
+                onChange={registerDataChange}
+              />
+            </div>
+            <button className="signUpBtn">Register</button>
+          </form>
         </div>
       </div>
     </>
