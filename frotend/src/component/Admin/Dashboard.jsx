ff67a247232f2eabf9b869ad988dcca552b9aa2b
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-03-23 12:09:00 +0530

    High chart  in dashborad added

--- frotend/src/component/Admin/Dashboard.jsx
+++ frotend/src/component/Admin/Dashboard.jsx
@@ -1,11 +1,147 @@
-import React from 'react'
-import "./Dashboard.css"
-
+import React, { useEffect } from "react";
+import "./Dashboard.css";
+import { Typography } from "@material-ui/core";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import { useSelector, useDispatch } from "react-redux";
+import { getAdminProducts, clearErrors } from "../../actions/productAction";
+import MetaData from "../layouts/MataData/MataData";
+import Loader from "../layouts/loader/Loader";
+import { useAlert } from "react-alert";
+import Sidebar from "./Siderbar";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
+  const dispatch = useDispatch();
+  const { products, loading, error } = useSelector((state) => state.products);
+  const alert = useAlert();
+  console.log("render");
+  let OutOfStock = 0;
+  products &&
+    products.forEach((element) => {
+      // check how much items out of stocks in products array
+      if (element.stock === 0) {
+        OutOfStock += 1;
+      }
+    });
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors);
+    }
+    console.log(loading);
+    dispatch(getAdminProducts());
+  }, [dispatch, error, alert]);
+
+  // chart js values for Line component
+  const lineOptions = {
+    chart: {
+      type: "line",
+    },
+    xAxis: {
+      categories: ["Initial Amount", "Amount Earned"],
+    },
+    yAxis: {
+      title: {
+        text: null,
+      },
+    },
+    series: [
+      {
+        name: "TOTAL AMOUNT",
+        data: [0, 2000],
+      },
+    ],
+  };
+
+  // now set the Value of stock of the product for Doughnut component in  chart .
+  const doughnutOptions = {
+    chart: {
+      type: "pie",
+    },
+    title: {
+      text: "Product Stock Status",
+    },
+    series: [
+      {
+        name: "Products",
+        data: [
+          {
+            name: "Out of Stock",
+            y: 2,
+            color: "#00A6B4",
+          },
+          {
+            name: "In Stock",
+            y: products.length - OutOfStock,
+            color: "#6800B4",
+          },
+        ],
+      },
+    ],
+  };
+
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {" "}
+          <div className="dashboard">
+            <MetaData title="Dashboard - Admin Panel" />
+            <Sidebar />
+
+            <div className="dashboardContainer">
+              <Typography component="h1">Dashboard</Typography>
+
+              <div className="dashboardSummary">
+                <div>
+                  <p>
+                    Total Amount <br /> ₹{2000}
+                  </p>
+                </div>
+                <div className="dashboardSummaryBox2">
+                  <Link to="/admin/products">
+                    <p>Product</p>
+                    <p>{products && products.length}</p>
+                  </Link>
+                  <Link to="/admin/orders">
+                    <p>Orders</p>
+                    <p>{0}</p>
+                  </Link>
+                  <Link to="/admin/users">
+                    <p>Users</p>
+                    <p>{0}</p>
+                  </Link>
+                </div>
+              </div>
+
+              <div className="lineChart">
+                <HighchartsReact
+                  highcharts={Highcharts}
+                  options={lineOptions}
+                />
+              </div>
+
+              <div className="doughnutChart">
+                <HighchartsReact
+                  highcharts={Highcharts}
+                  options={doughnutOptions}
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
+
+            
+            
+
+         
---
+++ frotend/src/component/Admin/Siderbar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "./Sidebar.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getAdminProducts, clearErrors } from "../../actions/productAction";
+import { useAlert } from "react-alert";
+import { Link } from "react-router-dom";
+import { TreeView, TreeItem } from "@material-ui/lab"; // used for down drop menu view as tree form
+import PostAddIcon from "@material-ui/icons/PostAdd";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import ImportExportIcon from "@material-ui/icons/ImportExport";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import PeopleIcon from "@material-ui/icons/People";
+import RateReviewIcon from "@material-ui/icons/RateReview";
+import logo from "../../Image/logo.png";
+
+/** 
+   @TreeView ,TreeItem used for Tree view drop down menu .
+  */
+function Sidebar() {
+  return (
+    <>
+      <div className="sidebar">
+        {/* //// Main logo */}
+        <Link to="/">
+          <img src={logo} alt="Ecart-logo" />
+        </Link>
+        {/* //// Dashboard icon */}
+        <Link to="/admin/dashboard">
+          <p>
+            <DashboardIcon />
+            Dashboard
+          </p>
+        </Link>
+        {/* //// Tree drom dwon menu for new Admin products */}
+        <Link>
+          <TreeView
+            defaultCollapseIcon={<ExpandMoreIcon />}
+            defaultExpandIcon={<ImportExportIcon />}
+          >
+            <TreeItem nodeId="1" label="Products">
+              <Link to="/admin/products">
+                <TreeItem nodeId="2" label="All" icon={<PostAddIcon />} />
+              </Link>
+
+              <Link to="/admin/product">
+                <TreeItem
+                  nodeId="3"
+                  label="Create"
+                  icon={<AddIcon />}
+                ></TreeItem>
+              </Link>
+            </TreeItem>
+          </TreeView>
+        </Link>
+        {/* ////orders */}
+        <Link to="/admin/orders">
+          <p>
+            <ListAltIcon />
+            Orders
+          </p>
+        </Link>
+        {/* Reviews */}
+        <Link to="/admin/reviews">
+          <p>
+            <RateReviewIcon />
+            Reviews
+          </p>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
