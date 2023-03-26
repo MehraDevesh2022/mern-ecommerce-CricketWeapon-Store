5233fe8fa15d085ebd209c6d5117d070bfd49eec
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-03-26 11:32:07 +0530

    cors and halmet added

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const errorMiddleware = require("./middleWare/error");
 const bodyParser = require("body-parser");
-// const cors = require("cors"); // Import cors module
+const cors = require("cors"); // Import cors module
 const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 const fileUpload = require("express-fileupload"); // used for image and other files
 require("dotenv").config({ path: "./config/config.env" });
 
 
+
+
+
+app.use(helmet());
 
 // routes
 const user = require("./route/userRoute");
@@ -18,7 +23,7 @@ const payment = require("./route/paymentRoute");
 // for req.cookie to get token while autentication
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(fileUpload());
