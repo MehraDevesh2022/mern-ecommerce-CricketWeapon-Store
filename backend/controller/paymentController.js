c5d7b824c9c53bab09f5fd970b9df7e2a1a89f9c
theluci <78475739+0dayhunter@users.noreply.github.com>
2023-11-06 18:25:37 +0530

    Update paymentController.js

--- backend/controller/paymentController.js
+++ backend/controller/paymentController.js
@@ -1,24 +1,20 @@
 const asyncWrapper = require("../middleWare/asyncWrapper");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // process the payment
 exports.processPayment = asyncWrapper(async (req, res, next) => {
-  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); // asigning key as well
-
   const myPayment = await stripe.paymentIntents.create({
-    amount: req.body.ammount,
+    amount: req.body.amount, // corrected typo "ammount" to "amount"
     currency: "inr",
     metadata: {
       company: "Ecommerce", // not mandatory
     },
   });
 
-  res
-    .status(200)
-    .json({ sucess: true, client_secret: myPayment.client_secret });
+  res.status(200).json({ success: true, client_secret: myPayment.client_secret });
 });
 
-// send STRIPE_API_KEY to user =>
-
+// send STRIPE_API_KEY to user
 exports.sendStripeApiKey = asyncWrapper(async (req, res, next) => {
   res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
 });
