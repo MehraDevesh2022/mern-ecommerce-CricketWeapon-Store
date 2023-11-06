079835ea57fc285cc03bc9e946f83696ce5258d2
theluci <78475739+0dayhunter@users.noreply.github.com>
2023-11-06 18:20:20 +0530

    Update paymentController.js

--- backend/controller/paymentController.js
+++ backend/controller/paymentController.js
@@ -3,26 +3,18 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // process the payment
 exports.processPayment = asyncWrapper(async (req, res, next) => {
-  try {
-    const myPayment = await stripe.paymentIntents.create({
-      amount: req.body.amount, // Corrected typo "ammount" to "amount"
-      currency: "inr",
-      metadata: {
-        company: "Ecommerce", // Not mandatory
-      },
-    });
+  const myPayment = await stripe.paymentIntents.create({
+    amount: req.body.amount, // corrected typo "ammount" to "amount"
+    currency: "inr",
+    metadata: {
+      company: "Ecommerce", // not mandatory
+    },
+  });
 
-    res.status(200).json({ success: true, client_secret: myPayment.client_secret });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
+  res.status(200).json({ success: true, client_secret: myPayment.client_secret });
 });
 
 // send STRIPE_API_KEY to user
 exports.sendStripeApiKey = asyncWrapper(async (req, res, next) => {
-  try {
-    res.status(200).json({ stripeApiKey: process.env.STRIPE_SECRET_KEY });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
+  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
 });
