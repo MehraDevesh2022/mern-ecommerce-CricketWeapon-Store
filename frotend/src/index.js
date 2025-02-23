2dc95d334b609fa979104ef75198a40228c0dfd2
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2025-02-23 23:58:00 +0530

    fix: update asset manifest and switch to HashRouter for improved routing

--- frotend/src/index.js
+++ frotend/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import { Provider } from "react-redux";
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import store from "./store";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom"; 
+// Replace BrowserRouter import with HashRouter
+import { HashRouter } from "react-router-dom";
 
 const theme = createTheme();
 const options = {
@@ -15,19 +17,20 @@ const options = {
   transition: transitions.SCALE,
 };
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <>
-    {/* Wrap the entire application with BrowserRouter */}
-    <BrowserRouter>
-    
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <AlertProvider template={AlertTemplate} {...options}>
-          <App />
-        </AlertProvider>
-      </Provider>
-    </ThemeProvider>
-    </BrowserRouter>
+    {/* Wrap the application with HashRouter for mobile routing support */}
+    <HashRouter>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <AlertProvider template={AlertTemplate} {...options}>
+            <App />
+          </AlertProvider>
+        </Provider>
+      </ThemeProvider>
+    </HashRouter>
   </>,
-  document.getElementById("root")
 );
